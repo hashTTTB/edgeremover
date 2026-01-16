8a91d9e4222dfec95eecac15a48ea40870ab1a9e
hash <hashwinmt@gmail.com>
2026-01-16 22:15:49 +0000

    Add files via upload

---
+++ script/edgeremover (run as admin).bat
@@ -0,0 +1,12 @@
+@ECHO ON
+rd/s/q "C:\Program Files (x86)\Microsoft\Edge"
+rd/s/q "C:\Program Files (x86)\Microsoft\EdgeCore"
+rd/s/q "C:\Program Files (x86)\Microsoft\EdgeUpdate"
+rd/s/q "C:\Program Files (x86)\Microsoft\EdgeWebview"
+rd/s/q "C:\Program Files (x86)\Microsoft\Temp"
+
+@ECHO OFF
+
+echo If you get Acces is denied, run as admin. If you get The system cannot find the file specified, edge might already be gone
+
+PAUSE
